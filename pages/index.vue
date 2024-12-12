f88c0559b19cc2cd32bbfb3d2f358c74a4ab8783
Mikhail <hramtsov13@mail.ru>
2024-12-12 19:43:36 +0400

    feat(icons): adds icons module

--- pages/index.vue
+++ pages/index.vue
@@ -19,25 +19,36 @@
 </script>
 
 <template>
-	<div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-		<div v-for="post in postsStore.posts" :key="post.id" class="rounded-md border border-gray-400 px-4 py-6 pb-20">
-			<h2 class="mb-2 text-sm font-semibold leading-4">
-				{{ post.title }}
-			</h2>
-
-			<p class="text-muted-foreground">
-				{{ post.body }}
+	<div>
+		<div>
+			<p>
+				Different sizes:
+				<NuxtIcon name="uil:github" />
+				<NuxtIcon name="uil:github" size="24" />
+				<NuxtIcon name="uil:github" size="48" />
 			</p>
 		</div>
 
-		<div v-for="user in postsStore.users" :key="user.id" class="rounded-md border border-gray-400 px-4 py-6 pb-20">
-			<h2 class="mb-2 text-sm font-semibold leading-4">
-				{{ user.name }}
-			</h2>
+		<div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+			<div v-for="post in postsStore.posts" :key="post.id" class="rounded-md border border-gray-400 px-4 py-6 pb-20">
+				<h2 class="mb-2 text-sm font-semibold leading-4">
+					{{ post.title }}
+				</h2>
 
-			<p class="text-muted-foreground">
-				{{ user.company.name }}
-			</p>
+				<p class="text-muted-foreground">
+					{{ post.body }}
+				</p>
+			</div>
+
+			<div v-for="user in postsStore.users" :key="user.id" class="rounded-md border border-gray-400 px-4 py-6 pb-20">
+				<h2 class="mb-2 text-sm font-semibold leading-4">
+					{{ user.name }}
+				</h2>
+
+				<p class="text-muted-foreground">
+					{{ user.company.name }}
+				</p>
+			</div>
 		</div>
 	</div>
 </template>
